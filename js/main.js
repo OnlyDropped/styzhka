418d518115b7322e8117851cd5bb4b46fa069b23
OnlyDropped <asheburenkov@yandex.ru>
2021-07-04 19:24:17 +0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('.flat__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  //$('.center').slick({
+  //  //centerMode: true,
+  //  //centerPadding: '0',
+  //  //slidesToShow: 3
+  //});
+});
